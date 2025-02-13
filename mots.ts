4e02487c9279f31078e22704524a234f76ddd89d
Insu-qg <quent.grange@gmail.com>
2025-02-13 11:50:40 +0100

    bump version to 0.1.3 and add documentation for string counting functions

--- mots.ts
+++ mots.ts
@@ -1,18 +1,47 @@
+/**
+ * This module contains functions to count things in strings.
+ * @module
+*/
+
+
+/**
+ * Counts the number of sentences in a given string.
+ * @param {string} str - The string to count sentences in.
+ * @returns {number} - The number of sentences in the string.
+*/
 export function nbMots(str:string): number {
   // renvoie le nombre de mots d'une chaine de caractère
   return str.trim().split(/\s+/).length;
 }
 
+
+ /**
+ * Counts the number of phrases in a given string.
+ * @param {string} str - The string to count phrases in.
+ * @returns {number} - The number of phrases in the string.
+*/
 export function nbPhrases(str:string): number {
   // renvoie le nombre de phrases d'une chaine de caractère
   return str.trim().split(/[.!?]/).length;
 }
 
+
+/**
+ * Counts the number of letters in a given string.
+ * @param {string} str - The string to count letters in.
+ * @returns {number} - The number of letters in the string.
+ */
 export function nbLettres(str: string): number {
   // renvoie le nombre de lettres d'une chaine de caractère
   return str.replace(/\s/g, '').length;
 }
 
+
+/**
+ * Counts the number of unique words in a given string.
+ * @param {string} str - The string to count unique words in.
+ * @returns {number} - The number of unique words in the string.
+ */
 export function nbRepetionsMots(str:string): number {
   // renvoie le nombre de répétitions de mots d'une chaine de caractère
   const mots = str.trim().split(/\s+/);
@@ -20,6 +49,12 @@ export function nbRepetionsMots(str:string): number {
   return mots.length - motsUniques.size;
 }
 
+
+/**
+ * Counts the number of unique letters in a given string.
+ * @param {string} str - The string to count unique letters in.
+ * @returns {number} - The number of unique letters in the string.
+ */
 export function nbRepetionsLettres(str:string): number {
   // renvoie le nombre de répétitions de lettres d'une chaine de caractère
   const lettres = str.replace(/\s/g, '');
@@ -27,6 +62,12 @@ export function nbRepetionsLettres(str:string): number {
   return lettres.length - lettresUniques.size;
 }
 
+
+/**
+ * Counts the number of unique sentences in a given string.
+ * @param {string} str - The string to count unique sentences in.
+ * @returns {number} - The number of unique sentences in the string.
+ */
 export function nbRépetitionsSymbole(str: string): number {
   // renvoie le nombre de répétitions de symboles d'une chaine de caractère
   const symboles = str.match(/[^\w\s]/g) || [];
